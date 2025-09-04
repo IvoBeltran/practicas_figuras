cf6d5204c09af03736e7920a4a14a2ebb6a7bdeb
unknown <ivonnenatalylozada@gmail.com>
2025-09-04 13:36:47 -0500

    Agregue la funcion para llamar al circulo

---
+++ js/main.js
@@ -0,0 +1,22 @@
+const figure = document.getElementById('figure');
+
+function circulo(){
+    figure.style.borderRadius = '50%';
+    figure.style.background = "lighblue";
+    
+}
+
+
+
+
+document.addEventListener('DOMContentLoaded', function(){
+    document.getElementById('btnCircle').addEventListener('click', circulo); // CIRCULOOO 
+
+
+
+
+
+    
+})
+
+
