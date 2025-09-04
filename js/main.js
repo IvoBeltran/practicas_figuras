65421fc84ce03c3f86a5116304c89447355c4da5
unknown <ivonnenatalylozada@gmail.com>
2025-09-04 13:38:48 -0500

    agregue la funcion de la figura de la estrella

--- js/main.js
+++ js/main.js
@@ -5,6 +5,12 @@ function circulo(){
     figure.style.background = "lighblue";
     
 }
+function estrella(){
+    figure.style.borderRadius = '0';
+    figure.style.clipPath = 'polygon(50% 0%, 61% 35%, 98% 35%, 68% 57%, 79% 91%, 50% 70%, 21% 91%, 32% 57%, 2% 35%, 39% 35%)';
+    figure.style.background ="lightblue";
+
+}
 
 
 
@@ -12,7 +18,7 @@ function circulo(){
 document.addEventListener('DOMContentLoaded', function(){
     document.getElementById('btnCircle').addEventListener('click', circulo); // CIRCULOOO 
 
-
+document.getElementById('btnStar').addEventListener('click', estrella ); // ESTRELLA 
 
 
 
